e4625f0c966939e0d320b55a3c0b3f308c00a70f
Alpy81 <alperen.ertas@gmx.de>
2024-01-26 21:52:15 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/github.com/Alpy81",
+  };
+
+  return config;
+});
